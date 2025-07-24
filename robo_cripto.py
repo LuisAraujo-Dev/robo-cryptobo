52a92afb0fba517a776bf1a976d1d00785c7187a
LuisAraujo-Dev <luisaraujo.dev@gmail.com>
2025-07-24 13:15:14 -0300

    feitas importacoes das bibliotecas e importacao das key de .env

---
+++ robo_cripto.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import os 
+import time
+
+from binance.client import Client
+from binance.enums import *
+
+api_key = os.getenv("KEY_BINANCE")
+api_key = os.getenv("SECRET_BINANCE")
+

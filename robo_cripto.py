f69ed4119f26defd698a5d0dc67aa997ec30558a
LuisAraujo-Dev <luisaraujo.dev@gmail.com>
2025-07-25 20:35:30 -0300

    criando requisicao de consulta de ativos

--- robo_cripto.py
+++ robo_cripto.py
@@ -1,11 +1,22 @@
 import pandas as pd
-import os 
+import os
 import time
-
+from dotenv import load_dotenv
 from binance.client import Client
 from binance.enums import *
+
+load_dotenv()
 
 api_key = os.getenv("KEY_BINANCE")
 secret_key = os.getenv("SECRET_BINANCE")
 
 cliente_binance = Client(api_key, secret_key)
+
+account = cliente_binance.get_account()
+
+lista_1 = ('BTC', 'ETH', 'SOL')
+
+for asset in account["balances"]:
+    if float(asset["free"]) > 0:
+    
+        print(asset)
